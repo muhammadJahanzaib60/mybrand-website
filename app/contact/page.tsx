3a4602d3c403c689dcc9fb74025b778a96d98767
muhammadJahanzaib60 <iknowbetter0077@gmail.com>
2026-07-18 15:11:10 +0500

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -9,7 +9,7 @@ export default function Contact(){
    <input name="name" required placeholder="Your Name" style={{width:"100%", padding:"12px", borderRadius:"8px", border:"1px solid #ddd", marginBottom:"10px"}}/>
    <input name="email" type="email" required placeholder="Email" style={{width:"100%", padding:"12px", borderRadius:"8px", border:"1px solid #ddd", marginBottom:"10px"}}/>
    <textarea name="message" required placeholder="Your Message" style={{width:"100%", padding:"12px", borderRadius:"8px", border:"1px solid #ddd", height:"100px"}}></textarea>
-   <button style={{width:"100%", marginTop:"10px", padding:"14px", background:"#2563EB", color:"#fff", border:"none", borderRadius:"8px", fontWeight:700}}>Send Message →</button>
+   <button style={{width:"100%", marginTop:"10px", padding:"14px" border:"none", borderRadius:"8px", fontWeight:700}}>Send Message →</button>
   </form>
  </section>
  )
